dede10aec42c8decd3db718e5f8f80d0e4fb44ca
Elisa Vaccaro <erv230@nyu.edu>
2017-09-13 20:37:16 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -28,7 +28,7 @@ return age > 12 && age < 20 ? 'You are a teenager' : 'You are not a teenager';
 
 function switchAge(age) {
   switch(age){
-  case > 12 && < 20:
+  case age > 12 && < 20:
   return 'You are a teenager';
   break;
 
