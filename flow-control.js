2c1cfb58bb9eacba29e55c62548a72605e145882
Elisa Vaccaro <erv230@nyu.edu>
2017-09-13 20:30:05 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -26,13 +26,13 @@ function ternaryTeenager(age) {
 return age > 12 && age < 20 ? 'You are a teenager' : 'You are not a teenager';
 }
 
-// function switchAge(age) {
-// switch(age){
-//   case: age > 12 && age < 20:
-//   return 'You are a teenager!';
-//   break;
-//
-//   default:
-//   return 'You have an age';
-// }
-// }
+function switchAge(age) {
+switch(age){
+  case: age > 12 && age < 20:
+  return 'You are a teenager!';
+  break;
+
+  default:
+  return 'You have an age';
+}
+}
